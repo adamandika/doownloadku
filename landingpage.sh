63ca28f2ee67f92fae4793890d1043abcac84297
adamandika <90945318+adamandika@users.noreply.github.com>
2021-10-03 14:35:43 +0700

    Delete landingpage.sh

--- landingpage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install apache2 -y
-sudo apt-get install unzip -y
-wget https://github.com/sdcilsy/landing-page/archive/master.zip
-unzip master.zip
-sudo rm /var/www/html/index.html
-sudo mv landing-page-master/* /var/www/html
